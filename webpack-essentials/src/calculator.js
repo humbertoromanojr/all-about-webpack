0d407f54a5b490eb912d6ed7c2cdc0572dd5a352
hjrdev <astronomi@gmail.com>
2022-02-13 12:55:36 -0300

    change path folder

--- webpack-essentials/src/calculator.js
+++
@@ -1,5 +0,0 @@
-const sum = function (a, b) {
-  console.log(a + b)
-}
-
-export default sum;
--- webpack-essentials/src/heading.js
+++
@@ -1,9 +0,0 @@
-class Heading {
-  create(text) {
-    const heading = document.createElement('he');
-    heading.innerText = text;
-    document.querySelector('body').appendChild(heading);
-  }
-}
-
-export default Heading;
